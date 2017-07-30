750ad8e3f816230f916fe2724b52d007556ede2a
summer12345 <1758524608@qq.com>
2017-07-30 16:58:11 +0800

    12

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>我是header</div>
+  <div>
+    <my-component></my-component>
+    <my-component></my-component>
+    <my-component></my-component>
+    <my-components></my-components>
+  </div>
 </template>
 <script type="text/ecmascript-6">
+  import Vue from 'vue'
+  // 注册
+
+  Vue.component('my-component', {
+      template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+      data: function () {
+          return {
+              counter: 0
+          }
+      }
+  })
+  var child = {
+    template: '<div>A custom </div>'
+  }
+  export default {
+      components: {
+          'my-components': child
+      },
+      data () {
+          return {
+
+          }
+      }
+  }
 
 </script>
 
